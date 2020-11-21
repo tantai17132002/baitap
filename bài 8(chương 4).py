9bff72e4e563d3453da30fb6e90f39b6748c8207
tantai17132002 <73747444+tantai17132002@users.noreply.github.com>
2020-11-21 14:50:35 +0700

    Add files via upload

---
+++ bài 8(chương 4).py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 14:46:42 2020
+
+@author: tanta
+"""
+
+a=input("địa chỉ email muốn gửi thư: ")
+b=input("thông điệp gửi thư: ")
+c=input("số lần gửi thư: ")
+counter=1
+n=int(input("nhập vào số lần lặp: "))
+while counter <= n: 
+       counter=counter+1
+       print("địa chỉ email muốn gửi thư: ",a)
+       print("thông điệp muốn gửi thư:     ",b)
+       print("số lần gửi thư:              ",c)
