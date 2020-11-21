c34333062e49a2af2c02580d0216bc8fce446da6
tantai17132002 <73747444+tantai17132002@users.noreply.github.com>
2020-11-21 20:04:03 +0700

    Add files via upload

---
+++ bài 2(chương 4).py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 23:33:03 2020
+
+@author: ADMIN
+"""
+
+a=int(input("a= "))
+b=int(input("b= "))
+x=-b/a
+print("x= ",str(x))
+A=("có dạng: ax+b=0")
+n=0
+for B in A:
+    n=n+1
+    print("a= ",a)
+    print("b= ",b)
+    print("x= ",x)
+    print("chương trình lặp giải phương trình bậc nhất n lần", A)
+    
